67a3df674a3bbdcd99183c459f138bd418dad7c6
surufel <henriquefrancasmm@gmail.com>
2025-10-22 00:17:54 -0300

    Adicionando um inteiro na lista encadeada (do tipo inteiro)

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Aprendendo Est. De Dados em Java.");
+        ListaEncadeada lista = new ListaEncadeada<Integer>();
+        lista.add(1);
+        System.out.println(lista);
+    }
+}
+
