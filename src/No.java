f0b7cfd7933cbdae65da17596c4a139062913b36
surufel <henriquefrancasmm@gmail.com>
2025-10-22 09:46:38 -0300

    Adicionando comentarios para facil compreensao para mim mesmo

--- src/No.java
+++ src/No.java
@@ -1,28 +1,39 @@
 public class No<T> {
     private T elemento;
     private No<T> proximo;
+
+    // Construtor do No (quando nao se sabe o proximo elemento)
     public No(T elemento) {
         this.elemento = elemento;
         this.proximo = null;
     }
+
+
+    // Construtor do No
     public No(T elemento, No<T> proximo) {
         this.elemento = elemento;
         this.proximo = proximo;
     }
 
+// Getters Setters
+
     public T getElemento() {
         return elemento;
     }
+
     public void setElemento(T elemento) {
         this.elemento = elemento;
     }
+
     public No<T> getProximo() {
         return proximo;
     }
+
     public void setProximo(No<T> proximo) {
         this.proximo = proximo;
     }
 
+    // Retorna o elemento ligado ao No e o proximo.
     @Override
     public String toString() {
         return "No {" + "elemento=" + elemento + ", proximo=" + proximo + '}';
