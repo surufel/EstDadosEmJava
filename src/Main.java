c297db087d26793ee500e81c5156167e2534b7f5
surufel <henriquefrancasmm@gmail.com>
2025-10-22 00:28:15 -0300

    Adicionando mais inteiros na lista para verificar aumento do tamanho da lista

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main{
         System.out.println("Aprendendo Est. De Dados em Java.");
         ListaEncadeada lista = new ListaEncadeada<Integer>();
         lista.add(1);
+        lista.add(1);
+        lista.add(1);
         System.out.println(lista);
+        System.out.println("Tamanho da lista:" + lista.getSize());
     }
 }
 
