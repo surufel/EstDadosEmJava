a4632020a6d51dcbb8b4f1b0076bc49b552b6ca6
surufel <henriquefrancasmm@gmail.com>
2025-10-22 10:23:58 -0300

    Visualizando a lista de fato, vendo os proximos elementos ate o nulo.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main{
         System.out.println("Aprendendo Est. De Dados em Java.");
         ListaEncadeada lista = new ListaEncadeada<Integer>();
         lista.add(1);
-        lista.add(1);
+        lista.add(2);
         lista.add(1);
         System.out.println(lista);
         System.out.println("Tamanho da lista:" + lista.getSize());
+        // Visualmente isso seria algo mais ou menos assim: [1]->[2]->[1]->NULL
+        // O sout da lista ja mostra isso.
     }
 }
 
