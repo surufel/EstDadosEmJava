55c472ace6bd0b4166b53b3e124927bcfbb779ed
surufel <henriquefrancasmm@gmail.com>
2025-10-22 11:21:16 -0300

    Agora quando imprime a lista, ele percorre a lista ligada e mostra todos os elementos (contando o nulo que fica fora da lista)

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Aprendendo Est. De Dados em Java.");
         ListaEncadeada lista = new ListaEncadeada<Integer>();
+        System.out.println(lista);
         lista.add(1);
         lista.add(2);
-        lista.add(1);
+        lista.add(3);
         System.out.println(lista);
         System.out.println("Tamanho da lista:" + lista.getSize());
-        // Visualmente isso seria algo mais ou menos assim: [1]->[2]->[1]->NULL
+        // Visualmente isso seria algo mais ou menos assim: [1]->[2]->[3]->NULL
         // O sout da lista ja mostra isso.
     }
 }
