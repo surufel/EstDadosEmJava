cb12c05a1d9617310d3096a0de1fb1fa4fcc42c5
surufel <henriquefrancasmm@gmail.com>
2025-10-22 00:27:35 -0300

    Int size foi adicionado pra saber o tamanho da lista, e o metodo getSize pra retornar o tamanho

--- src/ListaEncadeada.java
+++ src/ListaEncadeada.java
@@ -1,8 +1,14 @@
 public class ListaEncadeada<T> {
     private No<T> inicio;
+    private int size;
     public void add(T elemento){
         No<T> celula = new No<T>(elemento);
         this.inicio = celula;
+        this.size++; // Incrementa no construtor, pq para cada elemento add na instancia, o size aumenta +1
+    }
+
+    public int getSize(){
+        return this.size;
     }
     @Override
     public String toString() {
