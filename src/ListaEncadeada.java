ef236252dbcac38c511f2c7605c90c8df205febb
surufel <henriquefrancasmm@gmail.com>
2025-10-22 10:15:59 -0300

    Adicionado ultimo, e dentro do metodo add ele ja determina onde fica os elementos dependendo do size

--- src/ListaEncadeada.java
+++ src/ListaEncadeada.java
@@ -1,12 +1,18 @@
 public class ListaEncadeada<T> {
 
-    private No<T> inicio;
+    private No<T> inicio; // Acesso do elemento inicial
+    private No<T> ultimo; // Acesso do elemento final
     private int size;
 
     public void add(T elemento){ // Adiciona elemento na lista encadeada (NAO no final)
         No<T> celula = new No<T>(elemento);
-        this.inicio = celula;
+        if(this.size == 0){
+          this.inicio = celula; // O inicio vai ser o elemento
+        } else {
+            this.ultimo.setProximo(celula); // O ultimo elemento sera o proximo elemento
+        }
         this.size++; // Incrementa no construtor, pq para cada elemento add na instancia, o size aumenta +1
+        this.ultimo = celula;
     }
 
     public int getSize(){ // Retorna o tamanho da lista encadeada
