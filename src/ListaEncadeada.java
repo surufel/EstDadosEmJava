38e30d15eb425336cdf45bd0cbdced5e99d91467
surufel <henriquefrancasmm@gmail.com>
2025-10-22 11:19:09 -0300

    Segui o video da aula e fiz do metodo que foi ensinado, ele percorre dentro do toString

--- src/ListaEncadeada.java
+++ src/ListaEncadeada.java
@@ -4,10 +4,10 @@ public class ListaEncadeada<T> {
     private No<T> ultimo; // Acesso do elemento final
     private int size;
 
-    public void add(T elemento){ // Adiciona elemento na lista encadeada (NAO no final)
+    public void add(T elemento) { // Adiciona elemento na lista encadeada (NAO no final)
         No<T> celula = new No<T>(elemento);
-        if(this.size == 0){
-          this.inicio = celula; // O inicio vai ser o elemento
+        if (this.size == 0) {
+            this.inicio = celula; // O inicio vai ser o elemento
         } else {
             this.ultimo.setProximo(celula); // O ultimo elemento sera o proximo elemento
         }
@@ -15,24 +15,26 @@ public class ListaEncadeada<T> {
         this.ultimo = celula;
     }
 
-    public int getSize(){ // Retorna o tamanho da lista encadeada
+    public int getSize() { // Retorna o tamanho da lista encadeada
         return this.size;
-    }
-
-    public void percorrerLista(ListaEncadeada<T> lista){
-        System.out.println("PRIMEIRO ELEMENTO DA LISTA:" + this.inicio);
-        while(this.inicio != null){
-            this.inicio = inicio.getProximo();
-            System.out.println("PERCORRENDO . . . :" + this.inicio);
-            if (this.inicio.getProximo() == null){
-                System.out.println("NENHUM ELEMENTO ENCONTRADO, FIM DA LISTA");
-            }
-        }
     }
 
     @Override
     public String toString() { // Retorna o elemento inicial da lista encadeada
-        return "ListaEncadeada{" + "inicio= " + inicio + "}";
-    }
+        // "ListaEncadeada{" + "inicio= " + inicio + "}";
+        if (this.size == 0) {
+            return "[]";
+        }
 
+        StringBuilder builder = new StringBuilder("[");
+        No<T> atual = this.inicio;
+        builder.append(atual.getElemento()).append(",");
+
+        for (int i = 0; i < this.size - 1; i++) {
+            atual = atual.getProximo();
+            builder.append(atual.getElemento()).append(",");
+        }
+        builder.append(atual.getProximo()).append("]");
+        return builder.toString();
+    }
 }
