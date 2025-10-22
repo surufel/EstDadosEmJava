0877515851924e8fdefd300ff3c6b612b1df2348
surufel <henriquefrancasmm@gmail.com>
2025-10-22 09:44:16 -0300

    Adicionando comentarios para facil compreensao para mim mesmo

--- src/ListaEncadeada.java
+++ src/ListaEncadeada.java
@@ -1,19 +1,21 @@
 public class ListaEncadeada<T> {
+
     private No<T> inicio;
     private int size;
-    public void add(T elemento){
+
+    public void add(T elemento){ // Adiciona elemento na lista encadeada (NAO no final)
         No<T> celula = new No<T>(elemento);
         this.inicio = celula;
         this.size++; // Incrementa no construtor, pq para cada elemento add na instancia, o size aumenta +1
     }
 
-    public int getSize(){
+    public int getSize(){ // Retorna o tamanho da lista encadeada
         return this.size;
     }
+
     @Override
-    public String toString() {
-        return "ListaEncadeada{" +
-                "inicio=" + inicio +
-                '}';
+    public String toString() { // Retorna o elemento inicial da lista encadeada
+        return "ListaEncadeada{" + "inicio= " + inicio + "}";
     }
+
 }
