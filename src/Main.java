f7bd22dba3fe80afcd83ae581c555fa9e5ca6fe2
surufel <henriquefrancasmm@gmail.com>
2025-11-12 12:00:29 -0300

    Reapply "Fazendo busca de elementos e posicao na lista encadeada"
    This reverts commit 0b40c1f4a8f40dfa4f14e3187ab846f8e2c3ad28.

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,17 @@ public class Main{
         lista.clear();
         System.out.println(lista);
         System.out.println("Tamanho da lista:" + lista.getSize());
-        lista.add(1);
+        lista.add(10);
+        lista.add(20);
+        lista.add(30);
+        lista.add(40);
         System.out.println(lista);
         System.out.println("Tamanho da lista:" + lista.getSize());
         // Visualmente isso seria algo mais ou menos assim: [1]->[2]->[3]->NULL
         // O sout da lista ja mostra isso.
+        System.out.println("Buscando elemento 30 na lista acima");
+        System.out.println("Ele se encontra no indice: " + lista.buscaElemento(30) + " (Caso -1, nao encontrou o indice)");
+        System.out.println("Elemento na posicao 2: " + lista.buscaPosicao(2));
     }
 }
 
