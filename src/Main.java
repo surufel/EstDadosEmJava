d2cd47bdb49d1dc14204bc251f35077b7ade187d
surufel <henriquefrancasmm@gmail.com>
2025-11-12 14:39:28 -0300

    test Pilha + NavegacaoWeb

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,38 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Aprendendo Est. De Dados em Java.");
+        System.out.println("Aprendendo Est. de Dados em Java: Pilhas");
+        NavegacaoWeb web = new NavegacaoWeb();
+        web.visitarPagina("Youtube.com");
+        web.visitarPagina("Instagram.com");
+        web.visitarPagina("Spotify.com");
+        web.retrocederPagina();
+        web.retrocederPagina();
+        web.avancarPagina();
+        web.avancarPagina();
+
+        // Forçar limite
+        web.retrocederPagina();
+        web.retrocederPagina();
+        web.retrocederPagina();
+        web.retrocederPagina();
+
+        web.avancarPagina();
+        web.avancarPagina();
+        web.avancarPagina();
+        web.avancarPagina();
+        }
+}
+
+
+
+        /*System.out.println("Aprendendo Est. De Dados em Java.");
         ListaEncadeada lista = new ListaEncadeada<Integer>();
-        System.out.println(lista);
-        lista.add(1);
-        lista.add(2);
-        lista.add(3);
-        System.out.println(lista);
-        System.out.println("Tamanho da lista:" + lista.getSize());
-        lista.clear();
-        System.out.println(lista);
-        System.out.println("Tamanho da lista:" + lista.getSize());
         lista.add(10);
         lista.add(20);
         lista.add(30);
         lista.add(40);
         System.out.println(lista);
         System.out.println("Tamanho da lista:" + lista.getSize());
-        // Visualmente isso seria algo mais ou menos assim: [1]->[2]->[3]->NULL
-        // O sout da lista ja mostra isso.
         System.out.println("Buscando elemento 30 na lista acima");
         System.out.println("Ele se encontra no indice: " + lista.buscaElemento(30) + " (Caso -1, nao encontrou o indice)");
-        System.out.println("Elemento na posicao 2: " + lista.buscaPosicao(2));
-    }
-}
-
+        System.out.println("Elemento na posicao 2: " + lista.buscaPosicao(2));*/
